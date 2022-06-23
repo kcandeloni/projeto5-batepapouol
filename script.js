87a084966ffaee949a1711f127d9a446b0a2b399
kcandeloni <kcandeloni@gmail.com>
2022-06-22 23:56:37 -0300

    lógica get mensagens e post newMensagem

--- script.js
+++ script.js
@@ -18,10 +18,65 @@ const elementoQueQueroQueApareca = document.querySelector('.mensagem');
 elementoQueQueroQueApareca.scrollIntoView();
 */
 
-function enviaMensagem () {
-    
-}
+let nameUser = prompt("Informe sua Graça:");
+let mensagens;
+
+/*Testando Cód baseado no TasteCamp*/
+
 
 function scrolllMenu () {
     document.querySelector(".menuLateral").classList.toggle("visibility");
 }
+
+function buscarMensagem () {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(popularMensagem);
+}
+
+function popularReceitas(resposta) {
+    if (resposta.status === 200) {
+        console.log("Deuuu boooom");
+    }
+    mensagens = resposta.data;
+    renderizarMensagens();
+}
+
+function renderizarMensagens() {
+  const ul = document.querySelector(".mensagemArea");
+  ul.innerHTML = "";
+
+  for (let i = 0; i < mensagens.length; i++) {
+    ul.innerHTML += `
+        <li class="msn msnPrivate">${mensagens[i].message}
+        </li>`;
+  }
+}
+
+function enviaMensagem () {
+    const mensagem = document.querySelector(".newMensagem").value;
+  
+    const newMensagem = {
+    	from: nameUser,
+		/*to: "Todos",*/
+		text: mensagem,
+		type: "message",
+		time: "08:01:17"
+	};
+
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", newMensagem);
+
+    promise.then(buscarMensagem);
+    promise.catch(alertaErro);
+}
+
+    function alertaErro(error) {
+    if (error.response.status === 404) {
+        alert("Não foi encontrado!");
+    }
+    if (error.response.status === 422) {
+        alert("Verique todos os campos da receita!");
+    }
+      if (error.response.status === 409) {
+        alert("Já existe uma receita com esse título!");
+    }
+}   
